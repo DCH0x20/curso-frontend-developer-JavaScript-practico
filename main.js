1e1a3ccf330a48697b789ee38b848dc462720187
DCH0X20 <dlchh20@gmail.com>
2023-03-25 03:50:25 -0500

    unimos y agregamos javascript a mobile menu

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu)
+
 function toggleDesktopMenu(){
     //quita o pone la clase inactive dependiendo si ya lo tiene o no
     desktopMenu.classList.toggle('inactive');
@@ -12,4 +18,8 @@ function toggleDesktopMenu(){
         desktopMenu.classList.add('inactive')
     }
     */
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
